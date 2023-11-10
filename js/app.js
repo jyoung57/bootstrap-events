cda7f5c4effd51d567a03b52195ed916d8972bf2
Jon Young <jey6639@gmail.com>
2023-11-09 20:53:29 -0600

    added countdown timer to js

--- js/app.js
+++ js/app.js
@@ -67,3 +67,53 @@ function handleValidateSuggestion(event) {
     suggestionCheck.style.display = 'none';
   }
 }
+
+const timeElms = {
+  days: document.getElementById("days"),
+  hours: document.getElementById("hours"),
+  minutes: document.getElementById("minutes"),
+  seconds: document.getElementById("seconds"),
+};
+
+const countDownTimer = {
+  eventDate: new Date("2024-01-06T06:00:00").getTime(),
+  interval: null,
+  days: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+
+  start: function () {
+    this.interval = setInterval (() => {
+      const now = new Date().getTime();
+      const remainingTime = this.eventDate = now;
+
+      if (remainingTime <= 0) {
+        clearInterval(this.interval);
+        alert("Countdown has ended, thanks for stopping by.");
+      } else {
+        this.days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+        this.hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60 * 60));
+        this.seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+        this.render();
+      }
+    }, 1000);
+  },
+
+  stop: function () {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  },
+
+  render: function () {
+    timeElms.days.textContent = this.days;
+    timeElms.hours.textContent = this.hours;
+    timeElms.minutes.textContent = this.minutes;
+    timeElms.seconds.textContent = this.seconds;
+  },
+};
+
+countDownTimer.start();
